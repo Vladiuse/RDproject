7b248dbd9fe230ff59110cc0752c0e330b2deb58
Vladiuse <vladiuse@gmail.com>
2021-07-13 02:37:51 +0300

    fix labels admin.py, urls, add requirements

--- employees/urls.py
+++ employees/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('',views.employees, name='all'),
     # path('api/', views.employee_list),
     # path('api/<int:level_id>/', views.employee_level)
-    path('api/', views.EmployeeList.as_view()),
-    path('api/<int:level_id>/', views.EmployeeList.as_view())
+    path('api/', views.EmployeeList.as_view(), name='employee_all'),
+    path('api/<int:level_id>/', views.EmployeeList.as_view(), name='employee_level')
 ]
 
 
